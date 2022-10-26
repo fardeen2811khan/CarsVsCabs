0ba0e72d4e52cfebbcab2a32329345f8c2863003
Akash <belikeavkash@gmail.com>
2022-10-27 00:49:41 +0530

    car-data added

--- src/components/Cars.js
+++ src/components/Cars.js
@@ -1,24 +1,15 @@
 import React, { useEffect, useState } from 'react'
-
+import cars from '../DataBase/cardata'
 export default function Cars() {
-    const [cars,setCars] = useState([]);
-    const url =
-    "https://my-json-server.typicode.com/belikeakash/carsvsuber/cars";
-  const apidata = async () => {
-    const response = await fetch(url);
-    setCars(await response.json());
-    console.log(cars);
-  };
-  useEffect(()=> {
-    apidata();
-  },[])
+    
+  console.log(cars);
   return (
     <div>
         {
             cars.map((item)=> {
                 return (
                     <div>
-                        <h2>{item.name}</h2>
+                        <h2>Name : {item.name} Price : {item.price}</h2>
                     </div>
                 )
             })
@@ -27,3 +18,4 @@ export default function Cars() {
     </div>
   )
 }
+
